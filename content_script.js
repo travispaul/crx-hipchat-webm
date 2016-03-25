90d0c2bb91d33e67a41f54668a504eac4e962bbd
Travis Paul <Tr@visPaul.me>
2016-03-25 17:49:24 -0400

    Add icon, update content script

--- content_script.js
+++ content_script.js
@@ -2,8 +2,23 @@
 
     'use strict';
 
-    setInterval(function () {
-    	console.log(document.querySelectorAll('.file-video-default-poster'));
-    }, 1000);
+    function replaceVids() {
+    	setTimeout(function () {
+	    	var
+	    		spans = document.querySelectorAll('.file-video-default-poster:not(.webmify)'),
+	    		video;
+
+	    	for (var ea in spans) {
+	    		spans[ea].classList.add('webmify');
+	    		video = document.createElement('video');
+	    		video.src = spans[ea].parentElement.nextSibling.children[0].href;
+	    		video.autoplay = false;
+	    		video.controls = true;
+	    		spans[ea].parentNode.replaceChild(video, spans[ea]);
+	    	}
+    	}, 1000);
+    }
+
+	replaceVids();
 
 }());
