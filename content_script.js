e38931d99a89e448ea80ae75e921ed70f32e475d
Travis Paul <Tr@visPaul.me>
2016-03-26 13:40:59 -0400

    Tested, working

--- content_script.js
+++ content_script.js
@@ -3,22 +3,31 @@
     'use strict';
 
     function replaceVids() {
-    	setTimeout(function () {
-	    	var
-	    		spans = document.querySelectorAll('.file-video-default-poster:not(.webmify)'),
-	    		video;
+        setTimeout(function () {
+            var
+                selector = '.hc-chat-scrollbox .file-video-default-poster:not(.webmify)',
+                spans = document.querySelectorAll(selector),
+                video,
+                src;
 
-	    	for (var ea in spans) {
-	    		spans[ea].classList.add('webmify');
-	    		video = document.createElement('video');
-	    		video.src = spans[ea].parentElement.nextSibling.children[0].href;
-	    		video.autoplay = false;
-	    		video.controls = true;
-	    		spans[ea].parentNode.replaceChild(video, spans[ea]);
-	    	}
-    	}, 1000);
+            for (var ea = 0; ea < spans.length; ea += 1) {
+                spans[ea].classList.add('webmify');
+                src = spans[ea].parentElement.nextSibling.children[0].href;
+                if (src.split('.').pop() === 'webm') {
+                    video = document.createElement('video');
+                    video.src = src;
+                    video.autoplay = false;
+                    video.controls = true;
+                    spans[ea].parentNode.replaceChild(video, spans[ea]);
+                    delete spans[ea];
+                }
+            }
+
+            replaceVids();
+
+        }, 2500);
     }
 
-	replaceVids();
+    replaceVids();
 
 }());
