d4bb96226d420a11f8ca5fc4c935d10a343552d2
Travis Paul <Tr@visPaul.me>
2016-03-26 23:54:45 -0400

    Add batchSize

--- content_script.js
+++ content_script.js
@@ -1,33 +1,31 @@
-(function () {
+(function webmify() {
 
     'use strict';
 
-    function replaceVids() {
-        setTimeout(function () {
-            var
-                selector = '.hc-chat-scrollbox .file-video-default-poster:not(.webmify)',
-                spans = document.querySelectorAll(selector),
-                video,
-                src;
+    setTimeout(function () {
+        var
+            selector = '.hc-chat-scrollbox .file-video-default-poster:not(.webmify)',
+            spans = document.querySelectorAll(selector),
+            batchSize = 5,
+            video,
+            src,
+            ea;
 
-            for (var ea = 0; ea < spans.length; ea += 1) {
-                spans[ea].classList.add('webmify');
-                src = spans[ea].parentElement.nextSibling.children[0].href;
-                if (src.split('.').pop() === 'webm') {
-                    video = document.createElement('video');
-                    video.src = src;
-                    video.autoplay = false;
-                    video.controls = true;
-                    spans[ea].parentNode.replaceChild(video, spans[ea]);
-                    delete spans[ea];
-                }
+        for (ea = 0; ea < spans.length && ea < batchSize; ea += 1) {
+            spans[ea].classList.add('webmify');
+            src = spans[ea].parentElement.nextSibling.children[0].href;
+            if (src.split('.').pop() === 'webm') {
+                video = document.createElement('video');
+                video.src = src;
+                video.autoplay = false;
+                video.controls = true;
+                spans[ea].parentNode.replaceChild(video, spans[ea]);
+                delete spans[ea];
             }
+        }
 
-            replaceVids();
+        webmify();
 
-        }, 2500);
-    }
-
-    replaceVids();
+    }, 2500);
 
 }());
